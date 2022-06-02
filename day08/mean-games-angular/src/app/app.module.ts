934f645a2a1424419e274efba158873d8bc7bc1b
rolandovillca <rolandovillca@gmail.com>
2022-06-01 21:26:20 -0500

    fix service

--- day08/mean-games-angular/src/app/app.module.ts
+++ day08/mean-games-angular/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { NavigationComponent } from './navigation/navigation.component';
     NavigationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- day08/mean-games-angular/src/app/navigation/navigation.component.ts
+++ day08/mean-games-angular/src/app/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _router:Router) {}
 
   ngOnInit(): void {
   }
 
+  onHome():void {
+    this._router.navigate(['']);
+  }
+
+  onGames():void {
+    this._router.navigate(['']);
+  }
 }
